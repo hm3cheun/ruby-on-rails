426ab9865b4c1601d7536abe7e39c231d47243bd
Rails Udemy Course <hm3cheun83@gmail.com>
2016-01-22 00:21:33 +0000

    add testing, category model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit,:update,:show]
-    before_action :require_same_user, only:[:edit, :update]
-    
+    before_action :require_same_user, only:[:edit, :update, :destroy]
+    before_action :require_admin, only:[:destroy]
     
     def new
         @user = User.new
@@ -41,6 +41,13 @@ class UsersController < ApplicationController
     end
     
    
+    def destroy
+          @user = User.find(params[:id]) 
+          @user.destroy
+          session[:user_id] = nil
+          flash[:danger] ="user and all articles have been deleted"
+          redirect_to users_path
+    end
     
     private
     def user_params
@@ -52,10 +59,16 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-        if current_user != @user 
+        if current_user != @user && !current_user.admin?
         flash[:danger] =" you can not edit others profiles "
         redirect_to root_path
         end
     end
     
+    def require_admin
+        if logged_in? && !current_user.admin?
+        flash[:danger] =" you are not an admin"
+        redirect_to root_path
+        end 
+    end
 end
